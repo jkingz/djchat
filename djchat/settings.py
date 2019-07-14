600a970bf049b22c79c17cbcfc1d41d87b667238
jkingz <jfkrule@gmail.com>
2019-07-15 01:21:51 +0800

    Added channel layers

--- djchat/settings.py
+++ djchat/settings.py
@@ -76,6 +76,14 @@ WSGI_APPLICATION = 'djchat.wsgi.application'
 
 # Channels
 ASGI_APPLICATION = 'djchat.routing.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
